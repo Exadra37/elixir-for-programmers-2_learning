7a198500f9c4eec938b801a0ec0606a8df9d4174
Exadra37 <exadra37@gmail.com>
2022-03-13 21:45:58 +0000

    Let's Start Coding > Typespecs - Unit: 3/9 > Introduction to use Dyalizer for adding type specifications to the module functions
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/hangman/lib/hangman.ex
+++ hangman/hangman/lib/hangman.ex
@@ -1,9 +1,20 @@
 defmodule Hangman do
 
-  def new_game do
+  @state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type game :: any
+  @tally :: %{
+    turns_left: integer,
+    game_state: state,
+    letters: list(String.t),
+    used: list(String.t),
+  }
+
+  @spec new_game() :: game
+  def new_game() do
 
   end
 
+  @spec make_move(game, String.t) :: {game, tally}
   def make_move(game, guess) do
 
   end
