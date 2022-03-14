8b558314313e6829ba3b3acb9a303d01afa5831b
Exadra37 <exadra37@gmail.com>
2022-03-14 23:24:26 +0000

    Let's Start Coding > Check for Duplicate Moves - Unit: 7/9 > Added the game logic to detect that a letter was alrady used in a guess.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/hangman/test/impl_game_test.exs
+++ hangman/hangman/test/impl_game_test.exs
@@ -27,4 +27,28 @@ defmodule HangmanImplGameTest do
       assert new_game = game
     end
   end
+
+  test "a duplicate letter is reported" do
+    game = Game.new_game()
+
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+
+    {game, _tally} = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "we record letters used" do
+    game = Game.new_game()
+
+    {game, _tally} = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "y")
+    {game, _tally} = Game.make_move(game, "x")
+
+
+    assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
+  end
 end
