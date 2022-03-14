6523c377b1a2b0797735ecf596b90911682e5660
Exadra37 <exadra37@gmail.com>
2022-03-14 22:49:51 +0000

    Let's Start Coding > Pattern matching Game State - Unit: 6/9 > Added the game logic for win/lose game states with tests.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -1,8 +1,10 @@
 defmodule Hangman.Impl.Game do
 
+  alias Hangman.Type
+
   @type t :: %Hangman.Impl.Game{
     turns_left: integer,
-    game_state: Hangman.state(),
+    game_state: Type.state(),
     letters: [String.t()],
     used: MapSet.t(String.t())
   }
@@ -13,14 +15,30 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
+  @spec new_game() :: t
   def new_game() do
     Dictionary.random_word()
     |> new_game()
   end
 
+  @spec new_game(String.t) :: t
   def new_game(word) do
     %__MODULE__{
       letters: word |> String.codepoints()
+    }
+  end
+
+  @spec make_move(t, String.t) :: {t, Type.tally}
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used: game.used |> MapSet.to_list() |> Enum.sort()
     }
   end
 end
--- hangman/hangman/test/impl_game_test.exs
+++ hangman/hangman/test/impl_game_test.exs
@@ -8,7 +8,6 @@ defmodule HangmanImplGameTest do
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
-
   end
 
   test "new game returns correct word" do
@@ -17,6 +16,15 @@ defmodule HangmanImplGameTest do
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert game.letters == ["w", "o", "m", "b", "a", "t"]
+  end
 
+  test "state doesn't change if a game is won" do
+    for state <- [:won, :lost] do
+      game = Game.new_game("wombat")
+      game = Map.put(game, :game_state, :won)
+      {new_game, _tally} = Game.make_move(game, "x")
+
+      assert new_game = game
+    end
   end
 end
