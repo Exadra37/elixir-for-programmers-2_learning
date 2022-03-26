7f7b24a922854a694fb0bad050e9d48d826062d4
Exadra37 <exadra37@gmail.com>
2022-03-26 00:34:25 +0000

    No Mr Dictionary, I Expect You To Die > Add a Supervisor to Dictionary - Unit: 2/2 > Learned how start an Agent with a Supervisor.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/dictionary/lib/runtime/server.ex
+++ hangman/dictionary/lib/runtime/server.ex
@@ -5,10 +5,15 @@ defmodule Dictionary.Runtime.Server do
 
   @me __MODULE__
 
+  # Let's use the Agent behavior in order to allow for the Supervisor to know
+  # what it needs to start and run this Server, like the `child_spec/1` function
+  # that returns a specification to start an Agent under a Supervisor.
+  use Agent
+
   alias Dictionary.Impl.WordList
 
-  @spec start_link() :: {:ok, t}
-  def start_link() do
+  @spec start_link(list()) :: {:ok, t}
+  def start_link(_args) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
