614d105b13647377224c0368051ce62b108fbfd0
Exadra37 <exadra37@gmail.com>
2022-03-27 18:42:04 +0100

    Distributed Text clients > Don't Run Hangman Automatically - Unit: 2/3
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/text_client/mix.exs
+++ hangman/text_client/mix.exs
@@ -14,6 +14,7 @@ defmodule TextClient.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      included_applications: [:hangman],
       extra_applications: [:logger]
     ]
   end
