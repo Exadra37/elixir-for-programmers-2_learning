4d4b487af5123b0bf39d4676440cb169fec58923
Exadra37 <exadra37@gmail.com>
2022-03-30 00:53:59 +0100

    Hangman Using HTML Pages > More Tidying - Unit: 5/7
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/app/lib/app_web/views/hangman_view.ex
+++ hangman/app/lib/app_web/views/hangman_view.ex
@@ -1,6 +1,33 @@
 defmodule AppWeb.HangmanView do
   use AppWeb, :view
 
+  def continue_or_try_again(conn, status) when status in [:won, :lost] do
+    button("Try again", to: Routes.hangman_path(conn, :new))
+  end
+
+  def continue_or_try_again(conn, _status) do
+    form_for(conn, Routes.hangman_path(conn, :update), [ as: "make_move", method: :put ], fn f ->
+      [
+        text_input(f, :guess),
+        " ",
+        submit("Make next guess")
+      ]
+    end)
+  end
+
+  @status_fields %{
+    initializing: {"initializing", "Guess the word, a letter at a time"},
+    good_guess: {"good-guess", "Good guess!"},
+    bad_guess: {"bad-guess", "Sorry, that's a bad guess"},
+    won: {"won", "You won!"},
+    lost: {"lost", "Sorry, you lost!"},
+    already_used: {"already-used", "You already used that letter"},
+  }
+  def move_status(status) do
+    {class, msg} = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>"
+  end
+
   def figure_for(0) do
     ~s{
       ┌───┐
