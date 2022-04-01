bc967a9edce654708fc93cbf0383ccdb05f47917
Exadra37 <exadra37@gmail.com>
2022-04-01 01:20:06 +0100

    Updated the Hangman App to not die when the watchdog kills the game after 1 hour of inactivity.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/app/lib/app_web/controllers/hangman_controller.ex
+++ hangman/app/lib/app_web/controllers/hangman_controller.ex
@@ -6,24 +6,31 @@ defmodule AppWeb.HangmanController do
   end
 
   def new(conn, _params) do
-    game = Hangman.new_game()
-
-    conn
-    |> put_session(:game, game)
-    |> redirect(to: Routes.hangman_path(conn, :show))
+    _show_new_game(conn)
   end
 
-  def update(conn, %{"make_move" => %{"guess" => guess}} = params) do
-    put_in(conn.params["make_move"]["guess"], "")
-    |> get_session(:game)
-    |> Hangman.make_move(guess)
+  def update(conn, %{"make_move" => %{"guess" => guess}} = _params) do
 
-    # PragDave have not mention, but we are not using the updated conn from the
-    # pipeline above, and the game only works properly in :show because the
-    # :game we get from the session is in fact a pid for the Agent storing the
-    # game state, therefore when the tally for the game is retrieved in the
-    # :show action it will be up to date.
-    redirect(conn, to: Routes.hangman_path(conn, :show))
+    game = put_in(conn.params["make_move"]["guess"], "")
+      |> get_session(:game)
+
+    case Process.alive?(game) |> IO.inspect(label: "ALIVE?") do
+      true ->
+        game
+        |> Hangman.make_move(guess)
+
+        # PragDave have not mention, but we are not using the updated conn from the
+        # pipeline above, and the game only works properly in :show because the
+        # :game we get from the session is in fact a pid for the Agent storing the
+        # game state, therefore when the tally for the game is retrieved in the
+        # :show action it will be up to date.
+        redirect(conn, to: Routes.hangman_path(conn, :show))
+      false ->
+        conn
+        |> put_flash(:error, "Game restarted. You have been inactive for more then 1 hour.")
+        |> _show_new_game()
+    end
+
   end
 
   def show(conn, _param) do
@@ -35,4 +42,11 @@ defmodule AppWeb.HangmanController do
     render(conn, "game.html", tally: tally)
   end
 
+  defp _show_new_game(conn) do
+    game = Hangman.new_game()
+
+    conn
+    |> put_session(:game, game)
+    |> redirect(to: Routes.hangman_path(conn, :show))
+  end
 end
