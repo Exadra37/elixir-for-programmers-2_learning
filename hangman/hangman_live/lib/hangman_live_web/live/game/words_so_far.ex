cfd2c7e40b1448c90039ddb288f92941e8592569
Exadra37 <exadra37@gmail.com>
2022-04-02 23:37:53 +0100

    Hangman Using Liveview > Writing the WordSoFar Component - Unit: 2/4
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/hangman_live/lib/hangman_live_web/live/game/words_so_far.ex
+++ hangman/hangman_live/lib/hangman_live_web/live/game/words_so_far.ex
@@ -2,6 +2,15 @@ defmodule HangmanLiveWeb.Live.Game.WordsSoFar do
 
   use HangmanLiveWeb, :live_component
 
+  @states %{
+    already_used: "You already picked that letter",
+    bad_guess: "That's not in the word",
+    good_guess: "Good guess!",
+    initializing: "Type or click on your first guess",
+    lost: "sorry, you lost...",
+    won: "You won!",
+  }
+
   def mount(socket) do
     {:ok, socket}
   end
@@ -9,9 +18,24 @@ defmodule HangmanLiveWeb.Live.Game.WordsSoFar do
   def render(assigns) do
     ~H"""
     <div class="words-so-far">
-    WordsSoFar
+      <div class="words-so-far">
+         <%= state_name(@tally.game_state) %>
+      </div>
+
+      <div class="letters">
+      <%= for ch <- @tally.letters do %>
+        <% cls = if ch != "_", do: "one-letter correct", else: "one-letter" %>
+          <div class={cls}>
+            <%= ch %>
+          </div>
+      <% end %>
+      </div>
     </div>
     """
+  end
+
+  defp state_name(state) do
+    @states[state] || "Unknown state"
   end
 
 end
