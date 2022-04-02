14886981cbd7bbe892a6a8e536fec1a168aed5a3
Exadra37 <exadra37@gmail.com>
2022-04-03 00:13:23 +0100

    Hangman Using Liveview > Writing the Alphabet Component - Unit: 3/4
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/hangman_live/lib/hangman_live_web/live/game/alphabet.ex
+++ hangman/hangman_live/lib/hangman_live_web/live/game/alphabet.ex
@@ -3,15 +3,32 @@ defmodule HangmanLiveWeb.Live.Game.Alphabet do
   use HangmanLiveWeb, :live_component
 
   def mount(socket) do
+    letters = (?a..?z) |> Enum.map(fn char -> << char :: utf8 >> end)
+    socket = assign(socket, :letters, letters)
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
     <div class="alphabet">
-    Alphabet
+    <%= for letter <- assigns.letters do %>
+      <div
+        phx-click="make_move"
+        phx-value-key={letter}
+        class={"one-letter #{classOf(letter, @tally)}"}
+      >
+        <%= letter %>
+      </div>
+      <% end %>
     </div>
     """
   end
 
+  defp classOf(letter, tally) do
+    cond do
+      Enum.member?(tally.letters, letter) -> "correct"
+      Enum.member?(tally.used, letter) -> "wrong"
+      true -> ""
+    end
+  end
 end
