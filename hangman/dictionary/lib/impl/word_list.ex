358d74744dc09210f075c48b9a72ddeaadac4490
Exadra37 <exadra37@gmail.com>
2024-09-19 17:49:37 +0100

    Build the word list path at compile time.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- hangman/dictionary/lib/impl/word_list.ex
+++ hangman/dictionary/lib/impl/word_list.ex
@@ -3,13 +3,13 @@ defmodule Dictionary.Impl.WordList do
   @type t :: list(String)
   @type word :: String.t
 
-  @spec word_list() :: t
-  def word_list() do
-    "./../../assets/words.txt"
+  @word_list "./../../assets/words.txt"
     |> Path.expand(__DIR__)
     |> File.read!()
     |> String.split(~r/\n/, trim: true)
-  end
+
+  @spec word_list() :: t
+  def word_list(), do: @word_list
 
   @spec random_word(t) :: word
   def random_word(word_list)do
